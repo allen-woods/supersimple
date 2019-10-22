d06ae1cf2059026eef5c23f5333968984433e621
allen-woods <allen.woods@flatironschool.com>
2019-10-21 23:48:07 -0700

    update: added GoMongo into book and author example

--- resolver.go
+++ resolver.go
@@ -2,13 +2,32 @@ package supersimple
 
 import (
 	"context"
+	"log"
 	"time"
 
 	supersimple "github.com/allen-woods/supersimple/models"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	// "go.mongodb.org/mongo-driver/bson"
+	primitive "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+
+func GoMongo(db string, col string) (context.Context, *mongo.Collection) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	err = client.Ping(ctx, readpref.Primary())
+
+	if err != nil {
+		log.Fatal("No database was found. Is MongoDB running?")
+	}
+
+	collection := client.Database(db).Collection(col)
+
+	return ctx, collection
+}
 
 type Resolver struct{}
 
